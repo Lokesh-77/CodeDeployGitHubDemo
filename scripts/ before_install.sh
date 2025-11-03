cd39896e37d34c5f5aeccaa772e25113c3bf8fb2
Manoj <141417638+rajumanoj333@users.noreply.github.com>
2025-11-03 12:48:14 +0000

    Hello

---
+++ scripts/ before_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Remove old site files before new deployment
+echo "Cleaning old files..."
+rm -rf /var/www/html/*
---
+++ scripts/start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Simple log message
+echo "Deployment complete. App running at /var/www/html/index.html"
